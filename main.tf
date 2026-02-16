edabd49f794704ac961d6d77aefb25149891ae42
GyeongMin Yoon <152632692+opettikemi5@users.noreply.github.com>
2026-02-16 23:05:46 +0900

    update

--- main.tf
+++ main.tf
@@ -1,14 +1,21 @@
-provider "kubernetes" {
-  host                   = var.kubernetes_cluster_endpoint
-  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", var.kubernetes_cluster_name]
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+    }
   }
 }
 
 provider "helm" {
+  kubernetes {
+    host                   = var.kubernetes_cluster_endpoint
+    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", var.kubernetes_cluster_name]
+    }
+  }
 }
 
 provider "aws" {
